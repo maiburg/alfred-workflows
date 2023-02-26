359d80708668f22e278ceea88696b3487f82269c
Alex Maiburg <alex@maiburg.com>
2023-02-26 16:49:27 +0100

    asn ✨ expose fileHasContent

--- packages/alfred-lib/file-helper.js
+++ packages/alfred-lib/file-helper.js
@@ -1,21 +1,22 @@
 import * as fs from 'fs';
-import * as path from 'path';
 
 export const fileHelper = {
-  writeToFile: async (filePath, fileName, text) => {
-    const file = path.join(filePath, fileName);
-    text = fileExists(file) && fileHasContent(file) ? `\n${text}` : text;
+  writeToFile: async (file, text) => {
+    text =
+      fileExists(file) && fileHelper.fileHasContent(file) ? `\n${text}` : text;
+
     await fs.appendFile(file, text, (err) => {
       if (err) {
         return console.log(err);
       }
     });
+  },
+
+  fileHasContent: (file) => {
+    if (!fileExists(file)) return false;
+    const stats = fs.statSync(file);
+    return stats && stats.size;
   }
 };
 
 const fileExists = (file) => fs.existsSync(file);
-
-const fileHasContent = (file) => {
-  const stats = fs.statSync(file);
-  return stats && stats.size;
-};
