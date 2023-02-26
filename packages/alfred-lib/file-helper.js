1c98f44c1bf713c04c9104510b528cf24f5b6f6f
Alex Maiburg <alex@maiburg.com>
2023-02-26 20:02:23 +0100

    asn ✅ fileHelper more tests

--- packages/alfred-lib/file-helper.js
+++ packages/alfred-lib/file-helper.js
@@ -2,21 +2,16 @@ import * as fs from 'fs';
 
 export const fileHelper = {
   writeToFile: async (file, text) => {
-    text =
-      fileExists(file) && fileHelper.fileHasContent(file) ? `\n${text}` : text;
+    text = fileHelper.fileHasContent(file) ? `\n${text}` : text;
 
     await fs.promises.appendFile(file, text, (err) => {
-      if (err) {
-        return console.log(err);
-      }
+      err && console.log(err);
     });
   },
 
   fileHasContent: (file) => {
-    if (!fileExists(file)) return false;
+    if (!fs.existsSync(file)) return false;
     const stats = fs.statSync(file);
     return stats && stats.size;
   }
 };
-
-const fileExists = (file) => fs.existsSync(file);
--- packages/alfred-lib/tests/file-helper.spec.js
+++ packages/alfred-lib/tests/file-helper.spec.js
@@ -5,25 +5,47 @@ import { jest } from '@jest/globals';
 import { fileHelper } from '../file-helper.js';
 
 describe('fileHelper', () => {
-  describe('writeToFile should', () => {
-    const text = 'some text';
+  const file = `/tmp/notes.md`;
+  const text = 'some text';
 
+  describe('writeToFile() should', () => {
     test('call fileHasContent()', async () => {
       const spy = jest.spyOn(fileHelper, 'fileHasContent');
 
-      await fileHelper.writeToFile('file', text);
+      await fileHelper.writeToFile(file, text);
 
-      expect(spy).toHaveBeenCalledWith('file');
+      expect(spy).toHaveBeenCalledWith(file);
     });
 
-    test('call fs.appendFile()', async () => {
+    test('write to file', async () => {
       mock({ '/tmp': { 'notes.md': '' } });
-
-      const file = `/tmp/notes.md`;
 
       await fileHelper.writeToFile(file, text);
 
       expect(fs.readFileSync(file, 'utf8')).toEqual(text);
+    });
+
+    test('prepend a linebreak if file has content', async () => {
+      const expected = '1st line\n' + text;
+      mock({ '/tmp': { 'notes.md': '1st line' } });
+
+      await fileHelper.writeToFile(file, text);
+
+      expect(fs.readFileSync(file, 'utf8')).toEqual(expected);
+    });
+  });
+
+  describe('fileHasContent() should', () => {
+    test('return FALSE if file is empty', () => {
+      mock({ '/tmp': { 'notes.md': '' } });
+
+      expect(fileHelper.fileHasContent(file)).toBeFalsy();
+    });
+
+    test('return TRUE if file is NOT empty', () => {
+      mock({ '/tmp': { 'notes.md': 'some text' } });
+
+      expect(fileHelper.fileHasContent(file)).toBeTruthy();
     });
   });
 });
